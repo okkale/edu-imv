f1ba18242aacb657c1976b274bce0b6647a614e3
Omkar Kale <kaleomkar1011@gmail.com>
2026-07-20 13:13:29 +0530

    fix: increase payload limit to 50mb for media upload

--- assests/Asset-Manager/artifacts/api-server/src/app.ts
+++ assests/Asset-Manager/artifacts/api-server/src/app.ts
@@ -29,8 +29,8 @@ app.use(
 );
 
 app.use(cors({ origin: true, credentials: true }));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
 
 app.use(
